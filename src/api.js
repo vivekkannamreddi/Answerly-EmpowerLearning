eb82b3cc89ca752e6d80ae38c0a210bcf2679844
vivekkannamreddi <vivekkannamreddi@gmail.com>
2025-05-30 17:54:58 +0530

    updated the api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
-import react from "react"
-import axios from "axios"
+import axios from "axios";
 
-const API = axios.create({baseURL : "http://localhost:3000"})
-
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_BASE || "http://localhost:3000",
+});
 
 export default API;
