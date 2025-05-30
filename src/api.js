cccd3e6418e154002dbdb45fa2b350855a31d319
vivekkannamreddi <vivekkannamreddi@gmail.com>
2025-05-30 19:07:11 +0530

    updated api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE || "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_BASE ,
 });
+console.log(import.meta.env.VITE_API_BASE);
 
 export default API;
