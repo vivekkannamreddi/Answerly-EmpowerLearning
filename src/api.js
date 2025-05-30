984466f006af33bec0c40d9940442bb9103148fd
vivekkannamreddi <vivekkannamreddi@gmail.com>
2025-05-30 19:24:30 +0530

    .

--- src/api.js
+++ src/api.js
@@ -4,5 +4,6 @@ const API = axios.create({
   baseURL: import.meta.env.VITE_API_BASE ,
 });
 console.log(import.meta.env.VITE_API_BASE);
+console.log(import.meta.env.VITE_API_BASE);
 
 export default API;
