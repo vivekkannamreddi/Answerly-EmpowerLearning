634fbce7ef3cd45b216a9510027d55223491df14
vivekkannamreddi <vivekkannamreddi@gmail.com>
2025-05-30 19:34:56 +0530

    updated the cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,11 @@ dotenv.config();
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://answerly-empower-learning.vercel.app/",
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/auth", auth);
